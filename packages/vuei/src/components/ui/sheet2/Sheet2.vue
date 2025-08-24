ce5b05574cf83ebdfb77e5ad89ff18af11eb0fa7
Ahsath <ahsath@gmail.com>
2025-08-24 00:09:23 -0400

    Refactor Sheet2 component to improve teleport behavior and enhance modal functionality

--- packages/vuei/src/components/ui/sheet2/Sheet2.vue
+++ packages/vuei/src/components/ui/sheet2/Sheet2.vue
@@ -10,15 +10,21 @@ const sheet = useSheet(id);
 </script>
 
 <template>
-  <Teleport :to>
+  <Teleport :disabled="!sheet.isModal" :to>
     <div
       class="sheet2"
-      :class="{ 'sheet2--type-modal': sheet.isModal }"
+      :class="{
+        'sheet2--type-modal': sheet.isModal,
+        'sheet2--type-standard': !sheet.isModal,
+        'sheet2--open': sheet.isOpen,
+      }"
       :id="id"
       :role="sheet.isModal ? 'dialog' : undefined"
       :aria-modal="sheet.isModal ? 'true' : undefined"
     >
-      <div class="p-4">sheet2</div>
+      <div class="p-4">
+        <button @click="sheet.toggle">close</button>
+      </div>
     </div>
   </Teleport>
 </template>
