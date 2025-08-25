7eb8a8ccdf2c2997469ffa425d329f0459570f59
Ahsath <ahsath@gmail.com>
2025-08-25 18:01:06 -0400

    Refactor Sheet2 component to enhance focus trap functionality and improve modal scrim behavior

--- packages/vuei/src/components/ui/sheet2/Sheet2.vue
+++ packages/vuei/src/components/ui/sheet2/Sheet2.vue
@@ -1,17 +1,35 @@
 <script lang="ts" setup>
+import { useTemplateRef, watch } from "vue";
 import { useSheet } from "@/composables/useSheet";
+import { useFocusTrap } from "@vueuse/integrations/useFocusTrap";
 
 const { id, to = "body" } = defineProps<{
   id: string;
   to?: string;
 }>();
 
+const sheetRef = useTemplateRef("sheetRef");
 const sheet = useSheet(id);
+const { activate, deactivate } = useFocusTrap(sheetRef, {
+  clickOutsideDeactivates: true,
+});
+
+watch(
+  () => sheet.isOpen,
+  (isOpen) => {
+    if (isOpen && sheet.isModal) {
+      activate();
+    } else {
+      deactivate();
+    }
+  }
+);
 </script>
 
 <template>
   <Teleport :disabled="!sheet.isModal" :to>
     <div
+      ref="sheetRef"
       class="sheet2"
       :class="{
         'sheet2--type-modal': sheet.isModal,
@@ -27,7 +45,11 @@ const sheet = useSheet(id);
       </div>
     </div>
     <Transition name="fade">
-      <div v-show="sheet.isModal && sheet.isOpen" class="sheet2__scrim" />
+      <div
+        v-show="sheet.isModal && sheet.isOpen"
+        class="sheet2__scrim"
+        @click="sheet.toggle"
+      />
     </Transition>
   </Teleport>
 </template>
