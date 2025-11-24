a5f90cd176fe89c509362a3a4e8e54f588b9c32a
Ahsath <ahsath@gmail.com>
2025-11-23 22:38:51 -0400

    Fix Radio Button Styles
    Refine radio button styles to correctly reflect hover, focus, and active
    states. Adjust selectors to ensure proper application of background
    colors and icon styles based on the radio button's interaction state and
    checked status.

--- packages/vuei/src/components/base/forms/BaseRadio.vue
+++ packages/vuei/src/components/base/forms/BaseRadio.vue
@@ -6,7 +6,7 @@ defineOptions({ inheritAttrs: false });
 const attrs = useAttrs();
 
 // `value` is null to prevent a radio from being checked on mount (if not intentional)
-// due to how `v-model` works, after selecting a radio `v-model` will take the value of the `value` prop
+// due to how `v-model` works, after selecting a radio, `v-model` will take the value of the `value` prop
 // making it as if were checked but shouldn't
 const { value = null } = defineProps<{ value?: string }>();
 
