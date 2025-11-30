e1ea258c6c86c3aed834d9306ac5f3a07808cbf6
Ahsath <ahsath@gmail.com>
2025-11-30 13:21:52 -0400

    Fix trailing icon positioning for error state
    Move the error icon to the end of the trailing icons to ensure it's
    always visible.

--- packages/vuei/src/components/ui/input-field/InputFieldSimple.vue
+++ packages/vuei/src/components/ui/input-field/InputFieldSimple.vue
@@ -69,14 +69,14 @@ function focusInput() {
       <span v-if="suffix" class="input-field-simple__suffix">
         {{ suffix }}
       </span>
-      <div v-if="error" class="input-field-simple__trailing-icon">
-        <ErrorRounded aria-hidden="true" />
-      </div>
       <div
         v-else-if="$slots['trailing-icon']"
         class="input-field-simple__trailing-icon"
       >
         <slot name="trailing-icon" :disabled />
+      </div>
+      <div v-if="error" class="input-field-simple__trailing-icon">
+        <ErrorRounded aria-hidden="true" />
       </div>
       <div
         v-else-if="clearable && model"
